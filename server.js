1abb1253a4505c888d605d5e2a125c9c3d8b733f
Gaurav Gupta <gouravgupta840@gmail.com>
2024-07-31 13:16:00 +0530

    added cloudinary code

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const clodinary = require("cloudinary").v2;
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 const upload = require('./upload');
+
+clodinary.config({
+  cloud_name: "drc8sihlj",
+  api_key: "675897635184932",
+  api_secret: "04ziFqnmJJoNdg0JO2bjd4obQrE",
+});
 
 app.use(cors({
   origin: "*"
@@ -11,12 +18,25 @@ app.use(cors({
 
 app.post('/upload/single', upload.single('file'), (req, res) => {
   // Handle the uploaded file
-  res.json({ message: 'File uploaded successfully!' });
+  clodinary.uploader.upload(req.file.path, (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error uploading to Cloudinary');
+    }
+    res.json({
+      message: 'File uploaded successfully!',
+      secure_url: result.secure_url
+    });
+  });
 });
 
 app.post("/upload/multiple", upload.array("file", 10), (req, res) => {
   console.log(req.files)
   res.json({ message: 'Files uploaded successfully!' });
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Cloudinary");
 });
 
 app.listen(port, () => {
